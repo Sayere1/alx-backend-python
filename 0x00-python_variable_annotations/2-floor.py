c8eec67e46192a14fd3797559a923aea72616edd
Sayere1 <osayemwenreosaigbovo@gmail.com>
2024-06-07 08:00:32 +0000

    Annotation-floor/string, define variable

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+'''Typed annotation function'''
+
+
+def floor(x: float) -> int:
+    '''takes float as an arg and ret the floor of the float.'''
+    return int(x)
---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+'''Basic annotation - string'''
+
+
+def to_str(n: float) -> str:
+    '''takes a float n as argument and returns the string of the float.'''
+    return str(n)
